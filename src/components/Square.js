f4f385d2d24643b6e0cbb26351634700895afada
jimmytu <jimmytutron@gmail.com>
2018-06-25 20:04:05 -0700

    Add hover effect

--- src/components/Square.js
+++ src/components/Square.js
@@ -4,7 +4,7 @@ const Square = props => (
 
 	<div className = "col-12 col-md-4 col-lg-3 px-2 py-2">
       <div onClick={() => props.hitEmWitDat(props.id)} className="click">
-        <img className= "img-fluid" data-clicked={props.clicked} alt={props.name} src={props.image} />
+        <img className= "img-fluid hvr-pop" data-clicked={props.clicked} alt={props.name} src={props.image} />
       </div>
     </div> 
 
